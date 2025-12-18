e47765390633de3f38ca2cb2e56208cc75d54c19
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-18 16:57:32 +0100

    printed in console all string in names

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,7 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+for (let i=0; i<names.length; i++){
+    console.log(names[i]);
+}
