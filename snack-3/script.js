0c027aa581e6c260c9c9b03a16a63757ad654070
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-18 17:38:17 +0100

    snack-3: versione con for

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,10 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+// Versione semplice con ciclo for
+const numbersPlusOne = [];
+for (let i=0; i<numbers.length; i++){
+    numbersPlusOne.push(numbers[i] + 1);
+}
+ console.log(numbersPlusOne);
