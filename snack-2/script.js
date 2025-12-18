dd2c379c892869d27cf383615faf9480946b2932
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-18 17:19:45 +0100

    snak-2: versione-2

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,7 +1,8 @@
 const people = [
   { name: 'Paolo', age: 35 },
   { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: 'Marco', age: 67 },
+  { noName: 'Marco', age: 67 }
 ];
 
 // Stampa in console tutti i nomi
@@ -9,5 +10,14 @@ const people = [
 
 //Versione-1
 for (let i=0; i<people.length; i++){
-  console.log(people[i].name);
+  console.log("versione-1 ", people[i].name); //se un elemento non ha name, stampa undefined
+}
+
+//Versione-2
+for (const person of people){
+  for (const key in person) { //ciclo sulle proprietà della i-esima persona
+    if (key === `name`){ //se non c'è proprietà name, non stampo nulla
+      console.log("versione-2 ", person[key]);
+    }
+  } 
 }
