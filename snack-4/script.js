95e151713657e91ff0d28ac7a6814abd74e92c1b
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-18 17:49:24 +0100

    snack-4: con map()

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+// Usando il metodo map()
+const authors = posts.map( (post) => post.author );
+console.log(authors);
